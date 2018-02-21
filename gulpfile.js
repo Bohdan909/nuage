fec3e12e8dfc0c6a1b03511b2f3a9826da4d1d9d
Ilya Sopov <iluha.ua@gmail.com>
2018-02-21 15:40:35 +0200

    initial task for migration to gulp - sass

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function () {
+ return gulp.src('./sass/main.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(sourcemaps.write('./'))
+  .pipe(gulp.dest('./css'));
+});
