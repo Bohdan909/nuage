017afe6b9571adf7b77d221cc23706f2c4b76313
Ilya Sopov <iluha.ua@gmail.com>
2018-02-21 16:40:24 +0200

    cleancss minification added

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('sass', function () {
  return gulp.src('./sass/main.scss')
   .pipe(sourcemaps.init())
   .pipe(sass().on('error', sass.logError))
+  .pipe(cleanCSS({compatibility: 'ie10'}))
   .pipe(sourcemaps.write('./'))
   .pipe(gulp.dest('./css'));
 });
 
-gulp.task('default', function() {
-    gulp.run('sass');
-})
+gulp.task('default', ['sass']);
