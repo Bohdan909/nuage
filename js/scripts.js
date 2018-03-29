2d37816dae9f99822013c6073a03bf905c9aad60
Bohdan <bohdan.bilous2@gmail.com>
2018-03-29 13:21:59 +0300

    faq title animate

--- js/scripts.js
+++ js/scripts.js
@@ -1120,7 +1120,6 @@ document.documentElement.className = document.documentElement.className.replace(
                                     
                                     checkSliderEdge(currentSlideIndex);
                                 }
-                                
                             }	
                         });
                                 
